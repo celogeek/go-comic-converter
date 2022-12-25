6e37d41edfbe47786906c887584b21218c66bc49
celogeek <65178+celogeek@users.noreply.github.com>
2022-12-25 23:00:46 +0100

    readjust quality

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"fmt"
 	comicconverter "go-comic-converter/internal/comic-converter"
 )
 
 func main() {
-	comicconverter.
+	cv := comicconverter.
 		New(comicconverter.ComicConverterOptions{
-			Quality: 90,
-		}).
-		Load("/Users/vincent/Downloads/00001.jpg").
-		CropMarging().
-		Resize(1860, 2480).
-		GrayScale().
-		Save("/Users/vincent/Downloads/00001_gray.jpg")
+			Quality: 75,
+		})
+
+	for i := 1; i < 10; i++ {
+		cv.
+			Load(fmt.Sprintf("/Users/vincent/Downloads/0000%d.jpg", i)).
+			CropMarging().
+			Resize(1860, 2480).
+			Save(fmt.Sprintf("/Users/vincent/Downloads/0000%d_gray.jpg", i))
+	}
 
 }
