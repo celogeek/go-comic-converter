88eea07747ce1dcfbfa1b4b3f68a8132f230179f
celogeek <65178+celogeek@users.noreply.github.com>
2023-04-22 10:57:50 +0200

    rename files

---
+++ internal/epub/epub_templates.go
@@ -0,0 +1,21 @@
+package epub
+
+import _ "embed"
+
+//go:embed "templates/epub_templates_container.xml.tmpl"
+var containerTmpl string
+
+//go:embed "templates/epub_templates_applebooks.xml.tmpl"
+var appleBooksTmpl string
+
+//go:embed "templates/epub_templates_style.css.tmpl"
+var styleTmpl string
+
+//go:embed "templates/epub_templates_title.xhtml.tmpl"
+var titleTmpl string
+
+//go:embed "templates/epub_templates_text.xhtml.tmpl"
+var textTmpl string
+
+//go:embed "templates/epub_templates_blank.xhtml.tmpl"
+var blankTmpl string
